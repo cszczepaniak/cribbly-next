208b9c7f3cb31f814f93579f53cfca2813752849
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-07-03 13:42:25 -0500

    change spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,12 +19,12 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col items-center">
-        <h1 className="text-9xl py-6 font-bold">
+        <h1 className="text-9xl py-8 font-bold">
           Crib<span className="text-blue-700">b</span>
           <span className="text-red-600">l</span>
           <span className="text-green-600">y</span>
         </h1>
-        <h2 className="text-4xl text-slate-800">
+        <h2 className="text-4xl py-4 text-slate-800">
           2022 Szczepaniak Charity Cribbage Tournament
         </h2>
         <div className="flex flex-col items-center p-6">
