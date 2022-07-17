7a987c243167dfd13bd14564ddca1f1754add4d4
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-07-17 15:55:17 -0500

    take a stab at connecting things

---
+++ src/server/router/teams.ts
@@ -0,0 +1,57 @@
+import { createRouter } from "./context";
+import { z } from "zod";
+import { prisma } from "../db/client";
+
+export const teamsRouter = createRouter()
+  .mutation("create-team", {
+    async resolve() {
+      return await prisma.team.create({
+        data: {},
+      });
+    },
+  })
+  .mutation("add-player", {
+    input: z.object({
+      teamID: z.string().cuid(),
+      playerID: z.string().cuid(),
+    }),
+    async resolve({ input }) {
+      return await prisma.team.update({
+        where: {
+          id: input.teamID,
+        },
+        data: {
+          players: {
+            connect: {
+              id: input.playerID,
+            },
+          },
+        },
+      });
+    },
+  })
+  .mutation("remove-player", {
+    input: z.object({
+      teamID: z.string().cuid(),
+      playerID: z.string().cuid(),
+    }),
+    async resolve({ input }) {
+      return await prisma.team.update({
+        where: {
+          id: input.teamID,
+        },
+        data: {
+          players: {
+            disconnect: {
+              id: input.playerID,
+            },
+          },
+        },
+      });
+    },
+  })
+  .query("get-all-teams", {
+    async resolve() {
+      return await prisma.team.findMany();
+    },
+  });
