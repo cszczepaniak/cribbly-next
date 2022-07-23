854051ae5d6fecbde46897be53b159e23eab0a63
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-07-22 21:46:58 -0500

    add a query to only get full teams

--- src/server/router/teams.ts
+++ src/server/router/teams.ts
@@ -86,4 +86,49 @@ export const teamRouter = createRouter()
         },
       });
     },
+  })
+  .query("get-full", {
+    async resolve() {
+      type Player = {
+        id: string;
+        teamID: string;
+        firstName: string;
+        lastName: string;
+      };
+
+      type Team = {
+        id: string;
+        divisionID: string | null;
+        players: Player[];
+      };
+
+      type Result = { playerID: string } & Omit<Player, "id"> &
+        Omit<Team, "players">;
+
+      const resToPlayer = (r: Result) => ({
+        id: r.playerID,
+        teamID: r.teamID,
+        firstName: r.firstName,
+        lastName: r.lastName,
+      });
+
+      const result = await prisma.$queryRaw<Result[]>`
+      SELECT t.id, t.divisionID, p.id AS playerID, p.firstName, p.lastName FROM Team t INNER JOIN Player p ON t.id = p.teamID
+        WHERE (SELECT COUNT(*) FROM Player WHERE teamID = t.id) = 2;
+      `;
+
+      const teams = result.reduce((prev, r) => {
+        let t = prev.get(r.id);
+        if (!t) {
+          t = { id: r.id, divisionID: r.divisionID, players: [] };
+        }
+
+        t.players = [...t.players, resToPlayer(r)];
+
+        prev.set(t.id, t);
+        return prev;
+      }, new Map<string, Team>());
+
+      return Array.from(teams.values());
+    },
   });
