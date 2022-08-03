5740f03c81c77504b794b639ea1b8645ad3bd1a9
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-08-02 21:07:01 -0500

    fill out dummy tourney page

--- src/pages/admin/tournament.tsx
+++ src/pages/admin/tournament.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@components/styled-button";
 import { NextPage } from "next";
 
 const Tournament: NextPage = () => {
-  return <div>Tournament</div>;
+  return (
+    <div className="w-full flex flex-col items-center p-4">
+      <h1 className="text-5xl font-semibold pt-8 pb-8 text-center">
+        Tournament
+      </h1>
+      <Button className="max-w-md mb-8">Generate Prelim Games</Button>
+    </div>
+  );
 };
 
 export default Tournament;
