d17ffcd1e2b6ba763b13da71a7c3c94fe9d87a36
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-08-08 20:53:31 -0500

    move generate prelim button

--- src/pages/admin/games.tsx
+++ src/pages/admin/games.tsx
@@ -2,14 +2,31 @@ import { NextPage } from "next";
 import { trpc } from "utils/trpc";
 import { InferQueryOutput } from "@shared/utils/trpc-utils";
 import { getTeamName } from "@shared/utils/teams";
+import { Button } from "@components/styled-button";
 
 const Games: NextPage = () => {
+  const ctx = trpc.useContext();
   const { data, isLoading } = trpc.useQuery(["game.get-prelims"]);
+  const { mutate, error, isError } = trpc.useMutation("game.create-prelims", {
+    onSuccess: () => {
+      ctx.invalidateQueries(["game.get-prelims"]);
+    },
+  });
+
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-5xl font-semibold pt-8 pb-8 text-center">
         Manage Games
       </h1>
+      <Button
+        className="max-w-md mb-8"
+        onClick={() => {
+          mutate();
+        }}
+      >
+        Generate Prelim Games
+      </Button>
+      {isError && <p className="text-red-500">{error.message}</p>}
       <GameList isLoading={isLoading} data={data} />
     </div>
   );
--- src/pages/admin/tournament.tsx
+++ src/pages/admin/tournament.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@components/styled-button";
 import { NextPage } from "next";
 import { trpc } from "utils/trpc";
 
@@ -10,15 +9,6 @@ const Tournament: NextPage = () => {
       <h1 className="text-5xl font-semibold pt-8 pb-8 text-center">
         Tournament
       </h1>
-      <Button
-        className="max-w-md mb-8"
-        onClick={() => {
-          mutate();
-        }}
-      >
-        Generate Prelim Games
-      </Button>
-      {isError && <p className="text-red-500">{error.message}</p>}
     </div>
   );
 };
