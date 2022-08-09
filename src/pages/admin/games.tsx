1e83fc39c1d87761cdb5669c4ac9c56170e3755c
Connor Szczepaniak <cszczepaniak@gmail.com>
2022-08-08 20:59:57 -0500

    start to build out the openable thing

--- src/pages/admin/games.tsx
+++ src/pages/admin/games.tsx
@@ -3,6 +3,7 @@ import { trpc } from "utils/trpc";
 import { InferQueryOutput } from "@shared/utils/trpc-utils";
 import { getTeamName } from "@shared/utils/teams";
 import { Button } from "@components/styled-button";
+import { useState } from "react";
 
 const Games: NextPage = () => {
   const ctx = trpc.useContext();
@@ -22,20 +23,22 @@ const Games: NextPage = () => {
 
   return (
     <div className="flex flex-col items-center p-4">
-      <h1 className="text-5xl font-semibold pt-8 pb-8 text-center">
-        Manage Games
-      </h1>
-      <Button
-        disabled={prelimsCreating}
-        className="max-w-md mb-8 disabled:bg-blue-300 disabled:italic"
-        onClick={() => {
-          mutate();
-        }}
-      >
-        {prelimsCreating ? "Processing..." : "Generate Prelim Games"}
-      </Button>
-      {isError && <p className="text-red-500">{error.message}</p>}
-      <GameList isLoading={prelimsLoading} data={data} />
+      <div className="max-w-lg w-full">
+        <h1 className="text-5xl font-semibold pt-8 pb-8 text-center">
+          Manage Games
+        </h1>
+        <Button
+          disabled={prelimsCreating}
+          className="mb-8 disabled:bg-blue-300 disabled:italic"
+          onClick={() => {
+            mutate();
+          }}
+        >
+          {prelimsCreating ? "Processing..." : "Generate Prelim Games"}
+        </Button>
+        {isError && <p className="text-red-500">{error.message}</p>}
+        <GameList isLoading={prelimsLoading} data={data} />
+      </div>
     </div>
   );
 };
@@ -44,23 +47,29 @@ const GameList: React.FC<{
   isLoading: boolean;
   data: InferQueryOutput<"game.get-prelims"> | undefined;
 }> = ({ isLoading, data }) => {
+  const [showMore, setShowMore] = useState(false);
   if (isLoading || !data) {
     return <div className="text-lg">Loading games...</div>;
   }
   return (
-    <ul className="w-full max-w-lg">
-      {data.map((g, i) => (
-        <li
-          key={g.id}
-          className="flex flex-row space-y-4 justify-between items-center w-full"
-        >
-          <p>
-            {getTeamName(g.teams[0]?.players)} vs{" "}
-            {getTeamName(g.teams[1]?.players)}
-          </p>
-        </li>
-      ))}
-    </ul>
+    <div className="w-full">
+      <h3>Prelim Game List</h3>
+      {showMore && (
+        <ul className="w-full max-w-lg">
+          {data.map((g, i) => (
+            <li
+              key={g.id}
+              className="flex flex-row space-y-4 justify-between items-center w-full"
+            >
+              <p>
+                {getTeamName(g.teams[0]?.players)} vs{" "}
+                {getTeamName(g.teams[1]?.players)}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
