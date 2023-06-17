c1a5c96fb2c0549ec89b779c0f08846220e1b2ab
cszczepaniak <cszczepaniak@gmail.com>
2023-06-16 20:06:26 -0500

    fix build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 // src/pages/_app.tsx
 import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "../server/router";
-import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { Session } from "next-auth";
+import { AppProps } from "next/app";
 
-const MyApp: AppType = ({
+const MyApp = ({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
+}: AppProps<{
+  session: Session;
+}>) => {
   return (
     <SessionProvider session={session}>
       <DndProvider backend={HTML5Backend}>
